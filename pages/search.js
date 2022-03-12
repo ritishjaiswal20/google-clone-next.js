42cf90a448ff8a31ed5865c439ef1ebbddf039b2
Ritish jaiswal <ritishjaiswal20@gmail.com>
2022-03-12 23:06:55 +0530

    searchresult page completed

--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,21 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Header from "../components/Header";
+import SearchResults from "../components/SearchResults";
 import { API_KEY, CONTEXT_KEY } from "../keys";
+import Response from "../Response";
+
 function Search({ results }) {
+  const router = useRouter();
   console.log(results);
   return (
     <div>
       <Head>
-        <title>Search Result</title>
+        <title>{router.query.term}- Google Search</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <SearchResults results={results} />
     </div>
   );
 }
@@ -18,10 +24,12 @@ export default Search;
 
 export async function getServerSideProps(context) {
   const useDummyData = false;
-
-  const data = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`
-  ).then((response) => response.json());
+  const startIndex = context.query.start || "0";
+  const data = useDummyData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`
+      ).then((response) => response.json());
 
   return {
     props: {
